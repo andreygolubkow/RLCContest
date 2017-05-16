514b4a0cc1feb812036d3800a373e0396177187f
andreygolubkow <andreygolubkow@gmail.com>
2017-05-17 04:10:58 +0700

    Добавлено: Интерфейс IElement Классы Inductor и Resistor.

---
+++ RLCContest/Core/Elements/Inductor.cs
@@ -0,0 +1,105 @@
+﻿namespace Core.Elements
+{
+    using System;
+    using System.Numerics;
+
+    /// <summary>
+    /// The inductor.
+    /// </summary>
+    public class Inductor : IElement
+    {
+        /// <summary>
+        /// Наименование.
+        /// </summary>
+        private string _name;
+
+        /// <summary>
+        /// Индуктивность.
+        /// </summary>
+        private double _value;
+
+        /// <summary>
+        /// Создает новый <see cref="Inductor"/>.
+        /// С нулевым номиналом и наименованием Inductor.
+        /// </summary>
+        public Inductor()
+        {
+            Value = 0;
+            Name = "Inductor";
+        }
+
+        /// <summary>
+        /// Создает новый <see cref="Inductor"/>.
+        /// </summary>
+        /// <param name="name">
+        /// Наименование компонента.
+        /// </param>
+        /// <param name="value">
+        /// Номинал.
+        /// </param>
+        public Inductor(string name, double value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        #region Implementation of IElement
+
+        /// <summary>
+        /// Вызывается при изменении номинала.
+        /// </summary>
+        public event EventHandler ValueChanged;
+
+        /// <summary>
+        /// Название элемента.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+
+            set
+            {
+                if (value.Length == 0)
+                {
+                    throw new ArgumentException("Наименование катушки не может быть пустым.");
+                }
+
+                this._name = value;
+            }
+        }
+
+        /// <summary>
+        /// Значение.
+        /// </summary>
+        public double Value
+        {
+            get
+            {
+                return this._value;
+            }
+
+            set
+            {
+                if ( value < 0 )
+                {
+                    throw new ArgumentException("Индуктивность не может быть отрицательной.");
+                }
+
+                this._value = value;
+                ValueChanged?.Invoke(this, new EventArgs());
+            }
+        }
+
+        /// <summary>
+        /// Рассчет сопротивления.
+        /// </summary>
+        /// <param name="frequency">Частота.</param>
+        /// <returns>Комплексное число.</returns>
+        public Complex CalculateZ(double frequency) => 2 * Math.PI * frequency * this.Value;
+
+        #endregion
+    }
+}
---
+++ RLCContest/Core/Elements/Resistor.cs
@@ -0,0 +1,106 @@
+﻿namespace Core.Elements
+{
+    using System;
+    using System.Numerics;
+
+    /// <summary>
+    /// Резистор.
+    /// </summary>
+    public class Resistor : IElement
+    {
+        /// <summary>
+        /// Наименование резистора.
+        /// </summary>
+        private string _name;
+
+        /// <summary>
+        /// Номинал резистора.
+        /// </summary>
+        private double _value;
+
+        /// <summary>
+        /// Пустой контруктор для <see cref="Resistor"/>.
+        /// Номинал = 0.
+        /// Наименование компонента:"Resistor".
+        /// </summary>
+        public Resistor()
+        {
+            this.Value = 0;
+            this.Name = "Resistor";
+        }
+
+        /// <summary>
+        /// Конструктор <see cref="Resistor"/>.
+        /// </summary>
+        /// <param name="name">
+        /// Наименование элемента.
+        /// </param>
+        /// <param name="value">
+        /// Сопротивление.
+        /// </param>
+        public Resistor(string name, double value)
+        {
+            this.Value = value;
+            this.Name = name;
+        }
+
+        #region Implementation of IElement
+
+        /// <summary>
+        /// Срабатывает при изменении сопротивления.
+        /// </summary>
+        public event EventHandler ValueChanged;
+
+        /// <summary>
+        /// Название элемента.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+
+            set
+            {
+                if (value.Length == 0)
+                {
+                    throw new ArgumentException("Наименование резистора не может быть пустым.");
+                }
+
+                this._name = value;
+            }
+        }
+
+        /// <summary>
+        /// Сопротивление резистора.
+        /// </summary>
+        public double Value
+        {
+            get
+            {
+                return this._value;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Сопротивление не может быть отрицательным.");
+                }
+
+                this._value = value;
+                this.ValueChanged?.Invoke(this, new EventArgs());
+            }
+        }
+
+        /// <summary>
+        /// Рассчет сопротивления.
+        /// </summary>
+        /// <param name="frequency">Частота.</param>
+        /// <returns>Сопростивление резистора, не зависит от частоты.</returns>
+        public Complex CalculateZ(double frequency) => new Complex(this._value, 0);
+
+        #endregion
+    }
+}
