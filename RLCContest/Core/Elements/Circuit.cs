b9c29cf86c78bdac6020c92a77679022f96afcde
andreygolubkow <andreygolubkow@gmail.com>
2017-05-22 16:24:40 +0700

    Начал создаение класса Цепи в папке элементов

---
+++ RLCContest/Core/Elements/Circuit.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Core.Elements
+{
+    class Circuit : IElement
+    {
+        private string _name;
+        private double _value;
+
+        /// <summary>
+        /// Дорожка A и дорожка Б. Элементы должны подключатсья либо между ними, либо на одну дорожку
+        /// </summary>
+        private List<IElement> _aRoad;
+        private List<IElement> _bRoad;
+
+        public Circuit()
+        {
+            _aRoad = new List<IElement>();
+            _bRoad = new List<IElement>();
+        }
+
+        /// <summary>
+        /// Добавление парралельного элемента
+        /// </summary>
+        /// <param name="element"></param>
+        public void AddParrallel(IElement element)
+        {
+               
+        }
+
+        public void Add
+
+        #region Implementation of IElement
+
+        public event EventHandler ValueChanged;
+
+        /// <summary>
+        /// Название элемента.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        /// <summary>
+        /// Значение.
+        /// </summary>
+        public double Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+            }
+        }
+
+        /// <summary>
+        /// Рассчет сопротивления.
+        /// </summary>
+        /// <param name="frequency">Частота.</param>
+        /// <returns>Комплексное сопротивление.</returns>
+        public Complex CalculateZ(double frequency)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
