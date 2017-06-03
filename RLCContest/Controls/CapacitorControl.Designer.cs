0c4354663463519c62b3fa2bfd107968a6c74131
andreygolubkow <andreygolubkow@gmail.com>
2017-06-04 04:58:33 +0700

    Готов компонент Capacitor

--- RLCContest/Controls/CapacitorControl.Designer.cs
+++ RLCContest/Controls/CapacitorControl.Designer.cs
@@ -29,13 +29,10 @@
         private void InitializeComponent()
         {
             this.capacitorGroubBox = new System.Windows.Forms.GroupBox();
-            this.nameTextBox = new System.Windows.Forms.TextBox();
-            this.valueTextBox = new System.Windows.Forms.TextBox();
-            this.nameLabel = new System.Windows.Forms.Label();
             this.valueLabel = new System.Windows.Forms.Label();
-            this.calculateZButton = new System.Windows.Forms.Button();
-            this.zLabel = new System.Windows.Forms.Label();
-            this.zValueLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.valueTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
             this.capacitorGroubBox.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,48 +41,16 @@
             this.capacitorGroubBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.capacitorGroubBox.Controls.Add(this.zValueLabel);
-            this.capacitorGroubBox.Controls.Add(this.zLabel);
-            this.capacitorGroubBox.Controls.Add(this.calculateZButton);
             this.capacitorGroubBox.Controls.Add(this.valueLabel);
             this.capacitorGroubBox.Controls.Add(this.nameLabel);
             this.capacitorGroubBox.Controls.Add(this.valueTextBox);
             this.capacitorGroubBox.Controls.Add(this.nameTextBox);
             this.capacitorGroubBox.Location = new System.Drawing.Point(3, 0);
             this.capacitorGroubBox.Name = "capacitorGroubBox";
-            this.capacitorGroubBox.Size = new System.Drawing.Size(229, 92);
+            this.capacitorGroubBox.Size = new System.Drawing.Size(240, 65);
             this.capacitorGroubBox.TabIndex = 0;
             this.capacitorGroubBox.TabStop = false;
             this.capacitorGroubBox.Text = "Capacitor";
-            // 
-            // nameTextBox
-            // 
-            this.nameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.nameTextBox.Location = new System.Drawing.Point(47, 13);
-            this.nameTextBox.Name = "nameTextBox";
-            this.nameTextBox.Size = new System.Drawing.Size(175, 20);
-            this.nameTextBox.TabIndex = 0;
-            // 
-            // valueTextBox
-            // 
-            this.valueTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.valueTextBox.Location = new System.Drawing.Point(47, 39);
-            this.valueTextBox.Name = "valueTextBox";
-            this.valueTextBox.Size = new System.Drawing.Size(98, 20);
-            this.valueTextBox.TabIndex = 1;
-            // 
-            // nameLabel
-            // 
-            this.nameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.nameLabel.AutoSize = true;
-            this.nameLabel.Location = new System.Drawing.Point(6, 16);
-            this.nameLabel.Name = "nameLabel";
-            this.nameLabel.Size = new System.Drawing.Size(35, 13);
-            this.nameLabel.TabIndex = 2;
-            this.nameLabel.Text = "Name";
             // 
             // valueLabel
             // 
@@ -98,36 +63,34 @@
             this.valueLabel.TabIndex = 3;
             this.valueLabel.Text = "Value";
             // 
-            // calculateZButton
+            // nameLabel
             // 
-            this.calculateZButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.calculateZButton.Location = new System.Drawing.Point(148, 62);
-            this.calculateZButton.Name = "calculateZButton";
-            this.calculateZButton.Size = new System.Drawing.Size(75, 23);
-            this.calculateZButton.TabIndex = 4;
-            this.calculateZButton.Text = "Calculate";
-            this.calculateZButton.UseVisualStyleBackColor = true;
-            // 
-            // zLabel
-            // 
-            this.zLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.zLabel.AutoSize = true;
-            this.zLabel.Location = new System.Drawing.Point(6, 67);
-            this.zLabel.Name = "zLabel";
-            this.zLabel.Size = new System.Drawing.Size(14, 13);
-            this.zLabel.TabIndex = 5;
-            this.zLabel.Text = "Z";
-            // 
-            // zValueLabel
-            // 
-            this.zValueLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            this.nameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.zValueLabel.AutoSize = true;
-            this.zValueLabel.Location = new System.Drawing.Point(44, 67);
-            this.zValueLabel.Name = "zValueLabel";
-            this.zValueLabel.Size = new System.Drawing.Size(58, 13);
-            this.zValueLabel.TabIndex = 6;
-            this.zValueLabel.Text = "0 [Z value]";
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(6, 16);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(35, 13);
+            this.nameLabel.TabIndex = 2;
+            this.nameLabel.Text = "Name";
+            // 
+            // valueTextBox
+            // 
+            this.valueTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.valueTextBox.Location = new System.Drawing.Point(47, 39);
+            this.valueTextBox.Name = "valueTextBox";
+            this.valueTextBox.Size = new System.Drawing.Size(109, 20);
+            this.valueTextBox.TabIndex = 1;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.nameTextBox.Location = new System.Drawing.Point(47, 13);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(186, 20);
+            this.nameTextBox.TabIndex = 0;
             // 
             // CapacitorControl
             // 
@@ -135,7 +98,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.capacitorGroubBox);
             this.Name = "CapacitorControl";
-            this.Size = new System.Drawing.Size(238, 96);
+            this.Size = new System.Drawing.Size(249, 69);
             this.capacitorGroubBox.ResumeLayout(false);
             this.capacitorGroubBox.PerformLayout();
             this.ResumeLayout(false);
@@ -145,9 +108,6 @@
         #endregion
 
         private System.Windows.Forms.GroupBox capacitorGroubBox;
-        private System.Windows.Forms.Label zValueLabel;
-        private System.Windows.Forms.Label zLabel;
-        private System.Windows.Forms.Button calculateZButton;
         private System.Windows.Forms.Label valueLabel;
         private System.Windows.Forms.Label nameLabel;
         private System.Windows.Forms.TextBox valueTextBox;
--- RLCContest/Controls/CapacitorControl.cs
+++ RLCContest/Controls/CapacitorControl.cs
@@ -8,13 +8,45 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Core.Elements;
+
 namespace Controls
 {
-    public partial class CapacitorControl: UserControl
+    public partial class CapacitorControl : UserControl
     {
+        private Capacitor _element;
+
         public CapacitorControl()
         {
             InitializeComponent();
+            _element = null;
+#if !DEBUG
+            zValueLabel.Text = "";
+#endif
         }
+
+        [Browsable(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        [DefaultValue(null)]
+        public Capacitor Element
+        {
+            get
+            {
+                _element = new Capacitor(nameTextBox.Text,Convert.ToDouble(valueTextBox.Text));
+                return _element;
+            }
+            set
+            {
+                if ( value == null )
+                {
+                    nameTextBox.Clear();
+                    valueTextBox.Clear();
+                    return;
+                }
+                _element = value;
+            }
+        }
+
     }
 }
