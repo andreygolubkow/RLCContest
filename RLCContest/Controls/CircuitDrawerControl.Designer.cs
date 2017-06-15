df98b640df3f386fd04342b11d7c7022cd2fe3dc
andreygolubkow <andreygolubkow@gmail.com>
2017-06-15 08:04:33 +0700

    Готова простая отрисовка схемы

---
+++ RLCContest/Controls/CircuitDrawerControl.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Controls
+{
+    partial class CircuitDrawerControl
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridView
+            // 
+            this.gridView.AllowUserToAddRows = false;
+            this.gridView.AllowUserToDeleteRows = false;
+            this.gridView.AllowUserToResizeColumns = false;
+            this.gridView.AllowUserToResizeRows = false;
+            this.gridView.BackgroundColor = System.Drawing.Color.White;
+            this.gridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.gridView.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.gridView.ColumnHeadersHeight = 50;
+            this.gridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.gridView.ColumnHeadersVisible = false;
+            this.gridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridView.GridColor = System.Drawing.Color.White;
+            this.gridView.Location = new System.Drawing.Point(0, 0);
+            this.gridView.Name = "gridView";
+            this.gridView.ReadOnly = true;
+            this.gridView.RowHeadersVisible = false;
+            this.gridView.RowHeadersWidth = 50;
+            this.gridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.gridView.RowTemplate.Height = 50;
+            this.gridView.RowTemplate.ReadOnly = true;
+            this.gridView.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.gridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.gridView.Size = new System.Drawing.Size(433, 372);
+            this.gridView.TabIndex = 0;
+            // 
+            // CircuitDrawerControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gridView);
+            this.Name = "CircuitDrawerControl";
+            this.Size = new System.Drawing.Size(433, 372);
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridView;
+    }
+}
---
+++ RLCContest/Controls/CircuitDrawerControl.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Core;
+using Core.Circuits;
+using Core.Elements;
+
+using IComponent = Core.IComponent;
+
+namespace Controls
+{
+    public partial class CircuitDrawerControl : UserControl
+    {
+        public CircuitDrawerControl()
+        {
+            InitializeComponent();
+            var c = new SerialCircuit();
+            c.Add(new Capacitor("c1",5));
+            c.Add(new Resistor("r1", 1));
+            c.Add(new Inductor("i1", 2));
+            c.Add(new SerialCircuit());
+
+            DrawCircuit(c);
+        }
+
+        private void DrawCircuit(ICircuit circuit)
+        {
+            var upLine = SectionsCounter(circuit).upLine;
+            var vertLine = SectionsCounter(circuit).vertLine;
+            var downLine = SectionsCounter(circuit).downLine;
+
+            BuildArea(10, 10);
+
+            int x = 1;
+            int y = 1;
+            int upCounter = 0;
+            int vertCounter = 0;
+            int downCounter = 0;
+
+            foreach (IComponent component in circuit)
+            {
+                if ( upCounter < upLine )
+                {
+                    DrawComponent(x,y,component);
+                    x++;
+                    upCounter++;
+                    continue;
+                }
+                if ( vertCounter < vertLine )
+                {
+                    if ( vertCounter == 0 )
+                    {
+                        y++;
+                    }
+                    DrawComponent(x,y,component);
+                    vertCounter++;
+                    y++;
+                    continue;
+                }
+                if (downCounter < downLine)
+                {
+                    x--;
+                    DrawComponent(x, y, component);
+                    downCounter++;
+                }
+            }  
+
+        }
+
+        private static (int upLine,int vertLine,int downLine)  SectionsCounter(ICircuit circuit)
+        {
+            int upLine = circuit.Count % 3;
+            int downLine = upLine;
+            int vertLine = circuit.Count - upLine - downLine;
+            return (upLine, vertLine, downLine);
+        }
+
+        private void BuildArea(int columnsCount, int rowsCount)
+        {
+            gridView.RowCount = rowsCount;
+            for (int i = 0; i < columnsCount; i++)
+            {
+                gridView.Columns.Add(new DataGridViewImageColumn(false)
+                                     {
+                                         Width = 50
+                                     });
+            }
+        }
+
+        private void DrawComponent(int x, int y, Core.IComponent component)
+        {
+            var cell = (DataGridViewImageCell)gridView.Rows[y].Cells[x];
+            Image componentImage = null;
+            componentImage = component is Resistor ? CircuitImages.Resistor : componentImage;
+            componentImage = component is Capacitor ? CircuitImages.Capasitor : componentImage;
+            componentImage = component is Inductor ? CircuitImages.Inductor : componentImage;
+            componentImage = component is ICircuit ? CircuitImages.IC : componentImage;
+            cell.Value = componentImage;
+
+
+        }
+    }
+}
